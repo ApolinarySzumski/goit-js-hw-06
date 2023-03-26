db6e0b31319db973ac9548769fa2fb75cf1f542f
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-03-26 22:40:20 +0200

    finished 4 task

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+let counterValue = 0;
+
+const counterValueHTML = document.querySelector("#value");
+console.log(counterValueHTML);
+const decreaseButton = document.querySelector(`[data-action="decrement"]`);
+console.log(decreaseButton);
+const increaseButton = document.querySelector(`[data-action="increment"]`);
+console.log(increaseButton);
+
+const updateCounterValue = (newValue) => {
+  counterValue = newValue;
+  counterValueHTML.innerText = counterValue;
+};
+
+const increase = () => {
+  updateCounterValue(counterValue + 1);
+};
+
+const decrease = () => {
+  updateCounterValue(counterValue - 1);
+};
+
+increaseButton.addEventListener("click", increase);
+decreaseButton.addEventListener("click", decrease);
+
+console.log(counterValue);

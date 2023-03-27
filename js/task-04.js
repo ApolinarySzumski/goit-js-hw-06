d52a5cd9ab34a1a949f90c810757d029ee76031b
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-03-27 15:50:38 +0200

    finished 5th and 6th tasks

--- js/task-04.js
+++ js/task-04.js
@@ -24,3 +24,33 @@ increaseButton.addEventListener("click", increase);
 decreaseButton.addEventListener("click", decrease);
 
 console.log(counterValue);
+
+// Second version
+
+// let counterValue = 0;
+
+// const counterValueHTML = document.querySelector("#value");
+// console.log(counterValueHTML);
+// const decreaseButton = document.querySelector(`[data-action="decrement"]`);
+// console.log(decreaseButton);
+// const increaseButton = document.querySelector(`[data-action="increment"]`);
+// console.log(increaseButton);
+
+// const updateCounterValue = () => {
+//   counterValueHTML.innerText = counterValue;
+// };
+
+// const increase = () => {
+//   counterValue++;
+//   updateCounterValue();
+// };
+
+// const decrease = () => {
+//   counterValue--;
+//   updateCounterValue();
+// };
+
+// increaseButton.addEventListener("click", increase);
+// decreaseButton.addEventListener("click", decrease);
+
+// console.log(counterValue);
---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("#validation-input");
+const inputDataLength = Number(input.getAttribute("data-length"));
+
+console.log(input);
+console.log(typeof inputDataLength);
+console.log(inputDataLength);
+
+input.addEventListener("blur", (event) => {
+  if (event.currentTarget.value.length === inputDataLength) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+});
